df925a6901051e3eafb54234c844877affc58ee1
Dmitry Mikhalkin <dmitry_mihalkin@mail.ru>
2018-10-09 21:50:08 +0300

    Добвал вывод на экран через алерт

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 var celsium;
 var farengeit;
 
-celsium = prompt('Add a temperature in Celsium' )
+celsium = prompt('Add a temperature in Celsium' );
 farengeit = (9/5)*celsium + 32;
 console.log(farengeit);
+alert ('Температура в граудсах Цельсия составляет' + farengeit);
