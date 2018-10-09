05a0f70762470d644912df66f08a4f6673a1f443
Dmitry Mikhalkin <dmitry_mihalkin@mail.ru>
2018-10-09 21:51:41 +0300

    Изменил вывод на английский язык, т.к. русский в браузере как кракозябры

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,4 @@ var farengeit;
 celsium = prompt('Add a temperature in Celsium' );
 farengeit = (9/5)*celsium + 32;
 console.log(farengeit);
-alert ('Температура в граудсах Цельсия составляет' + farengeit);
+alert ('Temperature is ' + farengeit + ' C');
