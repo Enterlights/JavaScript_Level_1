5ee20f71f2359deacb16e78f75a17941bedfdedc
Dmitry Mikhalkin <dmitry_mihalkin@mail.ru>
2018-10-09 21:52:51 +0300

    изменил в выводе цельсии на фаренгейты

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,4 @@ var farengeit;
 celsium = prompt('Add a temperature in Celsium' );
 farengeit = (9/5)*celsium + 32;
 console.log(farengeit);
-alert ('Temperature is ' + farengeit + ' C');
+alert ('Temperature is ' + farengeit + ' F');
