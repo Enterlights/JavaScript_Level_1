544f46917e38b80cddd9adb73fccc2d0c0d12879
Dmitry Mikhalkin <dmitry_mihalkin@mail.ru>
2018-10-14 12:46:53 +0300

    add a solution for task #5

---
+++ scripts/task_5.js
@@ -0,0 +1,24 @@
+var a = +prompt('Введите число: '); //вводит переменную а с клавиатуры
+var b = +prompt('Введите число: '); //вводит переменную b с клавиатуры
+
+function sum ( a, b ) { //фукнция Сложение
+    return a + b;
+}
+
+function difference ( a, b ) { //функция Вычитание
+    return a - b;
+}
+
+function division ( a, b ) { //функция Деление
+    return a / b;
+}
+
+function multiplication ( a, b ) { //функция Умножение
+    return a * b;
+}
+
+//вызов функции
+console.log(sum(a, b));
+console.log(difference(a, b));
+console.log(division(a, b));
+console.log(multiplication(a, b));
